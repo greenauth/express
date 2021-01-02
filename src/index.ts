1ef37200da309e548cb7cd29136d42ce2265a261
Diego Rodriguez <dierodz91@gmail.com>
2021-01-02 15:12:31 -0300

    feat(auth/me): implement auth/me route

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ export default class GreenAuthServer {
   // Constructor
   constructor({ app }: { app: Application }) {
     this._app = app;
+    this._app.get("auth/me", (req: any, res) => {
+      if (!req.auth) return res.sendStatus(401);
+      return res.json({});
+    });
   }
 
   // Methods
